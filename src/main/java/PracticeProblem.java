4b83a6ada9f97c9cfc581e0a3100dd6f4806a65f
kelly-fu-ics4u1-02-25 <kelly.fu@student.tdsb.on.ca>
2025-04-08 13:22:38 +0000

    update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,8 +1,20 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
 	}
+
+	public static void insertionSort(char[] characters){
+		
+		for (int i = 1; i < characters.length; i++){
+			char currentValue = characters[i];
+			int j = i - 1;
+			while (j >= 0 && currentValue < characters[j]){
+				characters[j+1]=characters[j];
+				j--;
+			}
+			characters[j+1]= currentValue;
+		}
+	} 
 
 
 }
